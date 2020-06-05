eccec6615163176cad6ca67229b235b8147a0dc9
sergio amaya <saas02@gmail.com>
2020-06-05 13:51:50 -0500

    services web: V: 1.0.0.2: Validar el dominio para ejecutar la consulta del cliente

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -14,28 +14,32 @@ class GeneralController extends AbstractController
      */
     public function index()
     {
-        $session = new Session();
-        
-        if($session->has("agency_name") && $session->has("agency_info")){
+        $session = new Session();        
+        $result=[];
+        $dominio = $_SERVER['HTTP_HOST'];        
+        if($session->has("agency_name") && $session->has("agency_info")){            
             $result = json_decode($session->get("agency_info"), true);
-        }else{
-            $dominio = "www.tamatoly.com";
+            $renderFile = $result["nombre"]."/general/index.html.twig";
+        }else{            
             $cliente = $this->getDoctrine()->getRepository(Clientes::class)->findOneBy(["dominio" => $dominio, "estado" => 1]);
-            $result = [
-                "id" => $cliente->getId(),
-                "nombre" => $cliente->getNombre(),
-                "direccion" => $cliente->getDireccion(),
-                "telefono" => $cliente->getTelefono(),
-                "whatsapp" => $cliente->getWhatsapp(),
-                "facebook" => $cliente->getFacebook(),
-                "dominio" => $cliente->getDominio(),
-                "parametros" => $cliente->getParametros(),                
-            ];            
-            $session->set("agency_name", $cliente->getNombre());
-            $session->set("agency_info", json_encode($result));
-        }
-        
-        $renderFile = $result["nombre"]."/general/index.html.twig";
+            if(empty($cliente)){
+                $renderFile = "error.html.twig";               
+            }else{
+                $result = [
+                    "id" => $cliente->getId(),
+                    "nombre" => $cliente->getNombre(),
+                    "direccion" => $cliente->getDireccion(),
+                    "telefono" => $cliente->getTelefono(),
+                    "whatsapp" => $cliente->getWhatsapp(),
+                    "facebook" => $cliente->getFacebook(),
+                    "dominio" => $cliente->getDominio(),
+                    "parametros" => $cliente->getParametros(),                
+                ];            
+                $session->set("agency_name", $cliente->getNombre());
+                $session->set("agency_info", json_encode($result));
+                $renderFile = $result["nombre"]."/general/index.html.twig";
+            }            
+        }                
         
         return $this->render($renderFile, [ "result" => $result ]);
     }
